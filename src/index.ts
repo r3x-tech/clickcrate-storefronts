d9aa54d009b819bd71f690ad510806da11910d82
adrian-petrykiw <cucumber901@gmail.com>
2024-09-25 13:20:01 -0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import productAggregatorRoutes from "./routes/productAggregator";
 import { errorHandler } from "./middlewares/errorHandler";
+import { ACTIONS_CORS_HEADERS_MIDDLEWARE } from "@solana/actions";
 
 dotenv.config();
 
@@ -10,7 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(ACTIONS_CORS_HEADERS_MIDDLEWARE));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to ClickCrate Storefronts API!");
+});
 
 app.use("/api/products", productAggregatorRoutes);
 
--- src/routes/productAggregator.ts
+++ src/routes/productAggregator.ts
@@ -2,31 +2,48 @@ import express from "express";
 import { z } from "zod";
 import axios from "axios";
 import { PublicKey, Connection, clusterApiUrl } from "@solana/web3.js";
-import { ActionGetResponse, ACTIONS_CORS_HEADERS_MIDDLEWARE } from "@solana/actions";
+import {
+  ActionGetResponse,
+  ACTIONS_CORS_HEADERS_MIDDLEWARE,
+} from "@solana/actions";
 
 const router = express.Router();
-const CLICKCRATE_API_URL = process.env.CLICKCRATE_API_URL;
+const blinkCorsMiddleware = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  res.set(ACTIONS_CORS_HEADERS_MIDDLEWARE);
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({
+      body: "OK",
+    });
+  }
+  next();
+};
+router.use(blinkCorsMiddleware);
 
+const CLICKCRATE_API_URL = process.env.CLICKCRATE_API_URL;
 
 // Step 1: Choose product type
 router.get("/", (req, res) => {
-    console.log(req.query)
-    const payload: ActionGetResponse = {
-        icon: "https://shdw-drive.genesysgo.net/CiJnYeRgNUptSKR4MmsAPn7Zhp6LSv91ncWTuNqDLo7T/autofill_checkout_button_bottom.png",
-        label: "Choose a product",
-        type: "action",
-        title: "Product aggregator",
-        description: "A blink that allows you to aggregate multiple products",
-        links: {
-            actions: [
-                {
-                    href: "/api/products/shirt",
-                    label: "Shirt"
-                }
-            ]
-        }
-    }
-    res.status(200).json(payload);
+  console.log(req.query);
+  const payload: ActionGetResponse = {
+    icon: "https://shdw-drive.genesysgo.net/CiJnYeRgNUptSKR4MmsAPn7Zhp6LSv91ncWTuNqDLo7T/autofill_checkout_button_bottom.png",
+    label: "Choose a product",
+    type: "action",
+    title: "Product aggregator",
+    description: "A blink that allows you to aggregate multiple products",
+    links: {
+      actions: [
+        {
+          href: "/api/products/shirt",
+          label: "Shirt",
+        },
+      ],
+    },
+  };
+  res.status(200).json(payload);
 });
 
 export default router;
